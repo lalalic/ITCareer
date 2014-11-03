081a7ddac99ab6bbe9b8e90ec49056d028f45082
RaymondLi <lalalic@gmail.com>
2014-11-03 18:24:00 +0800

    dynamic content

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,48 @@
-require('cloud/app.js');
+var express = require('express');
+var app = express();
 
+app.set('views', 'cloud/views'); 
+app.set('view engine', 'ejs'); 
+app.use(express.bodyParser());
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var Post=Parse.Object.extend("post"), posts;
+(function init(){
+	var _=require('underscore')
+	Parse.initialize("CxZhTKQklDOhDasWX9hidldoK7xtzEmtcl5VSBeL","RwqvvbakVmWPhtO78QCUppfnclzfZ2SyUZ198ArG")
+	var query=new Parse.Query(Post)
+	query.descending('createdAt')
+	query.find({
+		success: function(results){
+			posts=resultes
+		},
+		error: function(error){
+		
+		})
+})();
+
+app.get('/(:title.html)?', function(req, res) {
+	var post=req.params.title ? posts.find() : posts.at(0)
+	res.render(req.query.edit ? 'post' : 'blog', post)
 });
+
+app.post('/(:title.html)?', function(req, res) {
+	if(req.body.id){
+		new Post({id:req.body.id})
+			.save({content:req.body.content})
+			.then(function(){
+				res.render('post', req.body)
+			}, function(error){
+				res.render('error', error)
+			})
+	}else{
+		new Post(req.body)
+			.save()
+			.then(function(post){
+				res.render('post', post)	
+			}, function(error){
+				res.render('error', error)
+			})
+	}
+});
+
+app.listen();
