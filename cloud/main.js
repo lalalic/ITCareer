5a6751e768acddafeeacc8152c739041e2d3c5db
RaymondLi <lalalic@gmail.com>
2014-11-06 18:01:20 +0800

    fix bugs

--- cloud/main.js
+++ cloud/main.js
@@ -1,54 +1,47 @@
 var express = require('express');
 var app = express();
-var ejs=require('ejs')
-ejs.debug=true
 
 app.set('views', 'cloud/views'); 
 app.set('view engine', 'ejs'); 
 app.use(express.bodyParser());
 
 var Post=Parse.Object.extend("post"), 
-	Posts=Parse.Collection.extend({model:Post}), 
-	loaded=false;
+	Posts=Parse.Collection.extend({model:Post});
 	
 var posts=new Posts();
 
-app.locals._=Parse._
+var _=app.locals._=Parse._
 app.locals.posts=posts
 app.locals.tags=false
 app.locals.site='{ITCareer} Blog'
 app.locals.siteDesc='Share knowledge about'
-app.locals.user='Li Cheng'
 app.locals.email='lalalic@139.com'
-
-
-
-function loadPosts(res){
-	if(loaded)
-		return Parse.Promise.as(posts)
-	return posts.fetch()
-			.then(function(){
-				loaded=true
-				console.log('found '+posts.length+' posts' )
-				return posts
-			},function (error){res.render('error', error)})
+app.locals.formatDate=function(d){
+	var d=new Date()
+	d.setTime(Date.parse(d))
+	return d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate()
 }
 
 app.get('/(:title.html)?', function(req, res) {
-	loadPosts(res).then(function(posts){
+	posts.fetch().then(function(posts){
 		var post=req.params.title ? posts.find(function(p){return p.get('title')==decodeURI(req.params.title)}) : posts.at(0);
-		res.render((req.query.edit ? 'post' : 'blog'), post.toJSON()||{title:'this post does NOT exist!'})
-	})
+		if(req.query.create!=undefined)
+			res.render('post',{title:'',content:'',objectId:'',tags:[]})
+		else
+			res.render((req.query.edit!=undefined ? 'post' : 'blog'), (post && post.toJSON())||{title:'this post does NOT exist!'})
+	},function (error){res.render('error', error)})
 });
 
 app.post('/(:title.html)?', function(req, res) {
-	loadPosts(res).then(function(posts){
+	posts.fetch().then(function(posts){
 		var post=req.body.objectId ? posts.get(req.body.objectId) : new Post()
 		if(req.body.tags)
 			req.body.tags=_.uniq(_.compact(req.body.tags.split(',')))
 		else
 			delete req.body.tags
-
+		
+		delete req.body.objectId
+		
 		if(post)
 			post.save(req.body)
 				.then(function(){
@@ -58,7 +51,7 @@ app.post('/(:title.html)?', function(req, res) {
 				},function (error){res.render('error', error)})
 		else
 			res.render('blog',{title:'this post does NOT exist!'})
-	})
+	},function (error){res.render('error', error)})
 });
 
 app.listen();
